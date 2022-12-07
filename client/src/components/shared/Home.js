0c7586eaf07ca4d8cc0e2da9f45194ac5ea5812b
Henry Doan <henrydoan96@gmail.com>
2022-12-06 20:54:28 -0700

    styling start

--- client/src/components/shared/Home.js
+++ client/src/components/shared/Home.js
@@ -1,11 +1,30 @@
 import Features from "./Features";
 import MainHead from "./MainHead";
 import Prices from "./Prices";
+import "../styles/Main.css";
+
+export const styles = {
+  prettyBtn: {
+    color: 'purple',
+    fontSize: '32px',
+    border: '1px dotted grey',
+  },
+  uglyBtn: {
+    color: 'orange',
+    fontSize: '12px',
+    border: '1px dotted yelloq',
+  }
+}
 
 const Home = () => (
   <>
     <MainHead title={"Welcome to TodoList!"} />
     <Features />
+    <button>Click</button>
+    <button className='pretty-btn'>Click</button>
+    <button style={{ color: 'green', fontSize: '24px', border: '1px solid brown'}}>Click</button>
+    <button style={styles.prettyBtn}>Click</button>
+    <button style={styles.uglyBtn}>Click</button>
     <Prices />
   </>
 )
